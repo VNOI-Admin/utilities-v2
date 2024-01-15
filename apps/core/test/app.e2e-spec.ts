348c9b60855a12ba9cccdccf506002eb747a5d56
Minh Hung Tran <hung3a8@gmail.com>
2024-01-15 01:36:00 +0100

    Setup turborepo

--- apps/core/test/app.e2e-spec.ts
+++ apps/core/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { CoreModule } from './../src/core.module';
 
-describe('AppController (e2e)', () => {
+describe('CoreController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [CoreModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
