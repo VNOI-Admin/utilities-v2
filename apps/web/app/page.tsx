1cdd4019fcede45909beae745a9ccac78f46a11f
winprn <winprn@github.com>
2024-01-20 23:06:36 +0700

    initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import VideoJS from "./components/VideoJS";
+import videojs from "video.js";
+
+export default function Page(): JSX.Element {
+  const playerRef = React.useRef(null);
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [{
+      src: 'http://192.168.2.16:9090/video.ogg',
+      type: 'video/ogg'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    });
+  };
+  return (
+    <>
+      <Navbar />
+      <main>
+        <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      
+       {/* <video width="1600" height="900" autoPlay loop id="video-background" muted>
+          <source src="http://192.168.2.16:9090/video.ogg" type="video/ogg" />
+        </video> */}
+      </main>
+    </>
+  );
+}
