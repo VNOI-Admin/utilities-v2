1d59d917a0d9b02dabe1e24a424f73ffe1236798
winprn <winprn@github.com>
2024-01-24 11:38:18 +0700

    fix cookie

--- apps/coach-view/app/api/api.ts
+++ apps/coach-view/app/api/api.ts
@@ -2,10 +2,13 @@ import { cookies } from 'next/headers';
 
 export const BASE_URL = 'http://localhost:8001';
 
-const getCurrentUser = () => cookies().get('user');
+const getCurrentUser = () => {
+  const cookie = cookies().get('user')?.value || '{}';
+  return JSON.parse(cookie);
+};
 
 export const makeHeaders = () => {
-  const user = JSON.parse(getCurrentUser().value);
+  const user = getCurrentUser();
 
   return new Headers({
     ...(!!user && { Authorization: `Bearer ${user.accessToken}` }),
