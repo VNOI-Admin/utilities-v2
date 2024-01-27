4c5f543d51135a709ae2c6794807f8844f3511aa
Trần Minh Hùng <hung3a8@gmail.com>
2024-01-27 03:48:41 +0100

    feat: setup ping task (#7)
    * feat: add ping task
    
    * fix: better log

---
+++ apps/user/src/task/task.service.ts
@@ -0,0 +1,43 @@
+import type { OnModuleInit} from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+import { Model } from 'mongoose';
+import * as ping from 'ping';
+
+import type { UserDocument } from '../database/schema/user.schema';
+import { User } from '../database/schema/user.schema';
+
+@Injectable()
+export class TaskService implements OnModuleInit {
+  private readonly logger = new Logger(TaskService.name);
+  constructor(
+    private schedulerRegistry: SchedulerRegistry,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async onModuleInit() {
+    const users = await this.userModel.find({ role: 'user', 'vpnIpAddress': { $ne: null }, isActive: true });
+    this.logger.debug(`Initializing ${users.length} cron jobs for users`);
+
+    for (const user of users) {
+      const job = new CronJob(CronExpression.EVERY_5_SECONDS, async () => {
+        const res = await ping.promise.probe(user.vpnIpAddress, {
+          timeout: 3,
+        });
+        if (res.alive && res.time != 'unknown') {
+          user.machineUsage.ping = res.time;
+          user.machineUsage.isOnline = true;
+        } else {
+          user.machineUsage.ping = 0;
+          user.machineUsage.isOnline = false;
+        }
+        user.save();
+      });
+      this.schedulerRegistry.addCronJob(`${user._id}-ping`, job);
+      job.start();
+    }
+  }
+}
--- apps/user/src/user/entities/User.entity.ts
+++ apps/user/src/user/entities/User.entity.ts
@@ -20,6 +20,10 @@ export class MachineUsageEntity {
 
   @Expose()
   @ApiProperty()
+  isOnline: boolean;
+
+  @Expose()
+  @ApiProperty()
   lastReportedAt: Date;
 };
 
