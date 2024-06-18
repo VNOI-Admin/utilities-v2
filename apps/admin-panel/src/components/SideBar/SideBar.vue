20108884df59c5b4b2241cb9e96e13605fc10e8c
BayMaxx2001 <trihien2001@gmail.com>
2024-06-19 01:39:24 +0700

    feat: sidebar component

--- apps/admin-panel/src/components/SideBar/SideBar.vue
+++ apps/admin-panel/src/components/SideBar/SideBar.vue
@@ -1,41 +1,60 @@
+<script setup lang="ts">
+import { Component } from "vue";
+
+const listBars = ref([
+  {
+    title: 'My Files',
+    value: 'my-files',
+    icon: 'mdi-folder',
+  },
+  {
+    title: 'Shared with me',
+    value: 'shared',
+    icon: 'mdi-account-multiple',
+  },
+  {
+    title: 'Starred',
+    value: 'starred',
+    icon: 'mdi-star',
+  },
+]);
+const props = defineProps<{
+  adminInfo: { avatar: string; gmail: number; name: string };
+}>();
+const contentProps = defineProps<{
+  content: Component;
+}>();
+</script>
+
+
 <template>
   <v-container fluid style="height: 100vh">
     <v-card>
       <v-navigation-drawer expand-on-hover rail>
         <v-list>
           <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
+            :prepend-avatar="adminInfo.avatar"
+            :subtitle="adminInfo.gmail"
+            :title="adminInfo.name"
           ></v-list-item>
         </v-list>
 
         <v-divider />
 
         <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-folder"
-            title="My Files"
-            value="myfiles"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-account-multiple"
-            title="Shared with me"
-            value="shared"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-star"
-            title="Starred"
-            value="starred"
-          ></v-list-item>
+          <p v-for="(item, index) in listBars" :key="index">
+            <v-list-item
+              color="primary"
+              :prepend-icon="item.icon"
+              :title="item.title"
+              :value="item.value"
+            ></v-list-item>
+          </p>
         </v-list>
       </v-navigation-drawer>
 
-      <v-main style=""><Table/></v-main>
+      <v-main style=""><content /></v-main>
     </v-card>
   </v-container>
 </template>
 
-<script setup lang="ts">
-//
-</script>
