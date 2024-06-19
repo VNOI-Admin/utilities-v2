c4cbbe4f5ed28e68541bcc4df9a63b8ef0483299
BayMaxx2001 <trihien2001@gmail.com>
2024-06-20 00:47:41 +0700

    feat: table component

--- apps/admin-panel/src/components/Table/Table.vue
+++ apps/admin-panel/src/components/Table/Table.vue
@@ -1,63 +1,79 @@
-<template>
-  <v-table theme="">
-    <thead>
-      <tr>
-        <th class="text-left">Name</th>
-        <th class="text-left">Problem</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in universities" :key="item.name">
-        <td>{{ item.name }}</td>
-        <td>{{ item.problem }}</td>
-      </tr>
-    </tbody>
-  </v-table>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      universities: [
-        {
-          name: 'FPT',
-          problem: 10,
-        },
-        {
-          name: 'UET',
-          problem: 9,
-        },
-        {
-          name: 'HUST',
-          problem: 8,
-        },
-        {
-          name: 'RMIT',
-          problem: 7,
-        },
-        {
-          name: 'VINU',
-          problem: 8,
-        },
-        {
-          name: 'HCMUS',
-          problem: 10,
-        },
-        {
-          name: 'HCMUT',
-          problem: 8,
-        },
-        {
-          name: 'HUE',
-          problem: 8,
-        },
-        {
-          name: 'PTIT',
-          problem: 8,
-        },
-      ],
-    };
-  },
+<script setup lang="ts">
+export type HeaderProps = {
+  align: 'start' | 'end';
+  key: string;
+  sortable: boolean;
+  title: string;
+  type: 'content' | 'image' | 'rating' | 'badge';
 };
+
+const props = defineProps<{
+  items: object[];
+  title: string;
+  headers: HeaderProps[];
+  pagination: boolean;
+}>();
+
+const search = ref('');
 </script>
+
+<template>
+  <v-card flat>
+    <v-card-title class="d-flex align-center pe-2">
+      <v-icon icon="mdi-video-input-component"></v-icon> &nbsp; {{ title }}
+
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        v-model="search"
+        density="compact"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo-filled"
+        flat
+        hide-details
+        single-line
+      ></v-text-field>
+    </v-card-title>
+
+    <v-divider></v-divider>
+    <v-data-table
+      v-model:search="search"
+      :items="items"
+      :headers="headers"
+      :items-per-page="itemsPerPage"
+    >
+      <template v-slot:[`item.image`]="{ item }">
+        <v-card class="my-2" elevation="2" rounded>
+          <v-img
+            :src="`https://cdn.vuetifyjs.com/docs/images/graphics/gpus/${item.image}`"
+            height="64"
+            cover
+          ></v-img>
+        </v-card>
+      </template>
+
+      <template v-slot:[`item.rating`]="{ item }">
+        <v-rating
+          :model-value="item.rating"
+          color="orange-darken-2"
+          density="compact"
+          size="small"
+          readonly
+        ></v-rating>
+      </template>
+
+      <template v-slot:[`item.badge`]="{ item }">
+        <div class="text">
+          <v-chip
+            :color="item.badge.color"
+            :text="item.badge.content"
+            class="text-uppercase"
+            size="small"
+            label
+          ></v-chip>
+        </div>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
--- apps/admin-panel/src/views/Admin.vue
+++ apps/admin-panel/src/views/Admin.vue
@@ -1,8 +1,99 @@
 <script setup lang="ts">
+import { HeaderProps } from '../components/Table/Table.vue';
+
 //
+const headers: HeaderProps[] = [
+  {
+    align: 'start',
+    key: 'name',
+    sortable: true,
+    title: 'NameTitle',
+    type: 'content',
+  },
+  {
+    align: 'start',
+    key: 'image',
+    sortable: false,
+    title: 'ImageTitle',
+    type: 'image',
+  },
+  {
+    align: 'start',
+    key: 'price',
+    sortable: true,
+    title: 'PriceTitle',
+    type: 'content',
+  },
+  {
+    align: 'start',
+    key: 'rating',
+    sortable: true,
+    title: 'RatingTitle',
+    type: 'rating',
+  },
+  {
+    align: 'start',
+    key: 'badge',
+    sortable: true,
+    title: 'StockTitle',
+    type: 'rating',
+  },
+];
+const items: object[] = [
+  {
+    name: 'Nebula GTX 3080',
+    image: '1.png',
+    price: 699.99,
+    rating: 5,
+    badge: {
+      color: 'green',
+      content:'out of stock'
+    },
+  },
+  {
+    name: 'Galaxy RTX 3080',
+    image: '2.png',
+    price: 799.99,
+    rating: 4,
+    badge: false,
+  },
+  {
+    name: 'Orion RX 6800 XT',
+    image: '3.png',
+    price: 649.99,
+    rating: 3,
+    badge: {
+      color: 'red',
+      content:'in stock'
+    },
+  },
+  {
+    name: 'Vortex RTX 3090',
+    image: '4.png',
+    price: 1499.99,
+    rating: 4,
+    badge: true,
+  },
+  {
+    name: 'Cosmos GTX 1660 Super',
+    image: '5.png',
+    price: 299.99,
+    rating: 4,
+    badge: false,
+  },
+];
+
+const items2 = [...items, ...items,...items]
+const title = 'Title Table';
+const pagination = true;
 </script>
 
 <template>
-    <SideBar/>
+  <Table
+    :headers="headers"
+    :items="items2"
+    :title="title"
+    :pagination="pagination"
+  />
 </template>
 
