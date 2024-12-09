4d07079031394bbbaa32f223547ba6c55edf4f7a
Minh Hung Tran <hung3a8@gmail.com>
2024-12-09 08:43:53 +0000

    fix: Fix Vue environment issue when build

--- apps/admin-panel/src/common/getEnv.ts
+++ apps/admin-panel/src/common/getEnv.ts
@@ -1,8 +1,9 @@
 import Config from '../../config/config.json';
 
 export default function getEnv(name: EnvNameKey) {
-  if (import.meta.env.MODE !== 'development')
-    return Config[name as ConfigKeyTypes];
+  console.log('MODE', import.meta.env.MODE);
+  console.log('CONFIG', Config);
+  if (import.meta.env.MODE !== 'development') return Config[name as ConfigKeyTypes];
 
   return import.meta.env[name as ConfigKeyTypes];
 }
