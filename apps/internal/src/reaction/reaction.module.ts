3eab2a3748bb7f05c5c461b1bb931494d2c1b48d
huytrinhm <huytrinhm@gmail.com>
2026-04-25 17:28:47 +0700

    fix: import UserModel to ReactionModule

--- apps/internal/src/reaction/reaction.module.ts
+++ apps/internal/src/reaction/reaction.module.ts
@@ -1,9 +1,12 @@
+import { User, UserSchema } from '@libs/common-db/schemas/user.schema';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ReactionController } from './reaction.controller';
 import { ReactionService } from './reaction.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [ReactionController],
   providers: [ReactionService],
 })
