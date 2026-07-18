59a5f80079d3e876ec60ba19ebc8613e430bbda7
huytrinhm <huytrinhm@gmail.com>
2026-07-18 23:07:00 +0700

    fix: missing UserModel in SystemConfigModule and RemoteControlModule

--- apps/internal/src/config/system-config.module.ts
+++ apps/internal/src/config/system-config.module.ts
@@ -1,10 +1,16 @@
 import { SystemConfig, SystemConfigSchema } from '@libs/common-db/schemas/systemConfig.schema';
+import { User, UserSchema } from '@libs/common-db/schemas/user.schema';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SystemConfigController } from './system-config.controller';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: SystemConfig.name, schema: SystemConfigSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: SystemConfig.name, schema: SystemConfigSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [SystemConfigController],
 })
 export class SystemConfigModule {}
--- apps/internal/src/remote-control/remote-control.module.ts
+++ apps/internal/src/remote-control/remote-control.module.ts
@@ -1,10 +1,16 @@
+import { User, UserSchema } from '@libs/common-db/schemas/user.schema';
 import { GuardsModule } from '@libs/common/guards/guards.module';
 import { RemoteControlCoreModule } from '@libs/common/remote-control/remote-control.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RemoteControlController } from './remote-control.controller';
 
 @Module({
-  imports: [GuardsModule, RemoteControlCoreModule],
+  imports: [
+    GuardsModule,
+    RemoteControlCoreModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [RemoteControlController],
   exports: [RemoteControlCoreModule],
 })
